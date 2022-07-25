c2cdb54ea746cf097b37d5d786f421b6adeac70f
River Place <rivspla@gmail.com>
2022-07-25 14:34:18 -0600

    Added HTML and initial factory functions & modules

---
+++ script.js
@@ -0,0 +1,22 @@
+const GameBoard = (() => {
+    /*
+    - Display gameboard in DOM
+    - Update board UI
+     */
+    const board = []
+
+})()
+
+const GameController = (() => {
+    /*
+    - Make Player move
+    - Reset game
+    - Determine winner or tie
+     */
+})()
+
+const Player = (sign) => {
+    /*
+    - Get sign
+     */
+}
